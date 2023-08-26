b2963856a25a77e9f622c1c592a09a73cdaebd8f
Tushar Pamnani <121151091+tusharpamnani@users.noreply.github.com>
2023-08-26 10:19:19 +0530

    Create constant.rs

---
+++ constant.rs
@@ -0,0 +1,9 @@
+use anchor_lang::prelude::*;
+
+#[constant]
+pub const USER_SEED: &[u8] = b"user";
+// u8 is unsigned integer 
+// b is binary string called user
+
+#[constant]
+pub const POST_SEED: &[u8] = b"post";
